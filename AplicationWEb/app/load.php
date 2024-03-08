6020f013b26614cbdad31715951e6b77e291a8a8
Leandro <131618950+LeandroCLOUD@users.noreply.github.com>
2024-03-08 16:41:18 -0300

    update load.php

--- AplicationWEb/app/load.php
+++ AplicationWEb/app/load.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>AWS Cloud Practioner Essentials</title>
+    <title>Netflix</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
   </head>
@@ -13,21 +13,11 @@
     		<div class="col-md-12">
       <?php include('menu.php'); ?>
       <div class="jumbotron" style="background-color:#555; color:#fff">
-
-     <h1>
-        Calma, segura a ansiedade!!!          
-     </h1>
-    <p>
-        Na AULA 3 (quinta-feira) você vai ver o <b>Pulo do Gato</b>.           
-     </p>
-    <p>
-        Já clica no link abaixo e define o lembrete na Aula 3 para não perder.           
-     </p>
-		  <a target="_blank" style="font-size:26px" href="https://www.youtube.com/live/aRDEazbRXaU?si=uKUSKynceBYe8K0Y"><b><u>ACESSAR AULA 3 NO YOUTUBE</u></b></a>
+   
 
 </br>
 <div class="embed-responsive embed-responsive-16by9">
-  .<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/aRDEazbRXaU?si=r8z7fNqprAkNAM5G" allowfullscreen=""></iframe>
+  .<iframe class="embed-responsive-item" src="https://www.netflix.com/br/" allowfullscreen=""></iframe>
 </div>
 
   </div>
